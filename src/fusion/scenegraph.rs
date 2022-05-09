cb0a9196040d22d3615da726385b34461feb2390
Nova <technobaboo@gmail.com>
2022-05-09 18:13:54 -0400

    fix(scenegraph): put nodes hashmap in refcell

--- src/fusion/scenegraph.rs
+++ src/fusion/scenegraph.rs
@@ -1,39 +1,41 @@
 use super::node::Node;
 use crate::{scenegraph, scenegraph::ScenegraphError};
-use std::{collections::HashMap, rc::Weak};
+use std::{cell::RefCell, collections::HashMap, rc::Weak};
 
 pub struct Scenegraph<'a> {
-	nodes: HashMap<String, Weak<Node<'a>>>,
+	nodes: RefCell<HashMap<String, Weak<Node<'a>>>>,
 }
 
 impl<'a> Scenegraph<'a> {
 	pub fn new() -> Self {
 		Scenegraph {
-			nodes: HashMap::new(),
+			nodes: RefCell::default(),
 		}
 	}
 
-	pub fn add_node(&mut self, node: Weak<Node<'a>>) {
+	pub fn add_node(&self, node: Weak<Node<'a>>) {
 		let node_ref = node.upgrade();
 		if node_ref.is_none() {
 			return;
 		}
 		self.nodes
+			.borrow_mut()
 			.insert(String::from(node_ref.unwrap().get_path()), node);
 	}
 
-	pub fn remove_node(&mut self, node: Weak<Node<'a>>) {
+	pub fn remove_node(&self, node: Weak<Node<'a>>) {
 		let node_ref = node.upgrade();
 		if node_ref.is_none() {
 			return;
 		}
-		self.nodes.remove(node_ref.unwrap().get_path());
+		self.nodes.borrow_mut().remove(node_ref.unwrap().get_path());
 	}
 }
 
 impl<'a> scenegraph::Scenegraph for Scenegraph<'a> {
 	fn send_signal(&self, path: &str, method: &str, data: &[u8]) -> Result<(), ScenegraphError> {
 		self.nodes
+			.borrow()
 			.get(path)
 			.ok_or(ScenegraphError::NodeNotFound)?
 			.upgrade()
@@ -48,6 +50,7 @@ impl<'a> scenegraph::Scenegraph for Scenegraph<'a> {
 		data: &[u8],
 	) -> Result<Vec<u8>, ScenegraphError> {
 		self.nodes
+			.borrow()
 			.get(path)
 			.ok_or(ScenegraphError::NodeNotFound)?
 			.upgrade()
