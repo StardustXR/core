b9c25a6e92b1669eddd779419ee2112fe33de6c7
Nova <technobaboo@gmail.com>
2022-05-08 23:22:52 -0400

    refactor(tests): get rid of dedicated tests file

--- src/tests.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn connect() {
-	let socket = super::client::connect().expect("Socket not connected");
-	let peer_addr = socket.peer_addr().expect("Couldn't get peer address");
-	println!(
-		"Socket peer address is {}",
-		peer_addr.as_pathname().unwrap().to_str().unwrap()
-	);
-}
