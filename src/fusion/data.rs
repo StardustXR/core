8021e558f1a79546286833f5749c9f08c1759843
Nova <technobaboo@gmail.com>
2022-06-15 03:33:09 -0400

    feat(pulse receiver): add test

--- src/fusion/data.rs
+++ src/fusion/data.rs
@@ -9,7 +9,7 @@ use crate::flex;
 
 pub struct PulseReceiver<'a> {
 	pub spatial: Spatial<'a>,
-	pub field: &'a Field<'a>,
+	// pub field: &'a Field<'a>,
 }
 
 impl<'a> PulseReceiver<'a> {
@@ -18,7 +18,7 @@ impl<'a> PulseReceiver<'a> {
 		spatial_parent: &Spatial<'a>,
 		position: values::Vec3,
 		rotation: values::Quat,
-		field: &'a Field<'a>,
+		field: &Field<'a>,
 	) -> Result<Self, NodeError> {
 		Ok(PulseReceiver {
 			spatial: Spatial {
@@ -35,7 +35,34 @@ impl<'a> PulseReceiver<'a> {
 					field.spatial.node.get_path()
 				),
 			},
-			field,
+			// field,
 		})
 	}
+}
+
+#[test]
+fn fusion_pulse_receiver() {
+	let mut client = Client::connect().expect("Couldn't connect");
+	let stopper = client.get_cross_thread_stopper();
+	let field = super::field::SphereField::create(
+		&client,
+		client.get_root(),
+		mint::Vector3::from([0_f32, 0_f32, 0_f32]),
+		0.1_f32,
+	)
+	.unwrap();
+	let pulse_receiver = PulseReceiver::create(
+		&client,
+		client.get_root(),
+		mint::Vector3::from([0_f32, 0_f32, 0_f32]),
+		mint::Quaternion::from([0_f32, 0_f32, 0_f32, 1_f32]),
+		&field.field,
+	)
+	.unwrap();
+	let wait_thread = std::thread::spawn(move || {
+		std::thread::sleep(core::time::Duration::from_secs(1));
+		stopper.stop()
+	});
+	client.run_event_loop(None).expect("Event loop failed");
+	let _ = wait_thread.join();
 }
