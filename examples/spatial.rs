8cd70c47d44c9724e7c49e9db1a4f5d1251c42fb
Nova <technobaboo@gmail.com>
2022-08-21 11:48:29 -0400

    feat(examples): spatial demo

---
+++ examples/spatial.rs
@@ -0,0 +1,114 @@
+use anyhow::Result;
+use glam::{vec3, Quat};
+use libstardustxr::fusion::{
+	async_trait,
+	client::{Client, LifeCycleHandler, LogicStepInfo},
+	drawable::Model,
+	resource::Resource,
+	spatial::Spatial,
+};
+use manifest_dir_macros::directory_relative_path;
+use std::sync::Arc;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+	let (client, event_loop) = Client::connect_with_async_loop().await?;
+	client
+		.set_base_prefixes(&[directory_relative_path!("res")])
+		.await?;
+
+	let base = Arc::new(SpatialDemo::new(&client).await?);
+	client.set_life_cycle_handler(&base);
+
+	tokio::select! {
+		_ = tokio::signal::ctrl_c() => Ok(()),
+		_ = event_loop => Err(anyhow::anyhow!("Server crashed")),
+	}
+}
+
+struct SpatialDemo {
+	client: Arc<Client>,
+	root: Spatial,
+	gem: Model,
+	ring_inner: Model,
+	ring_middle: Model,
+	ring_outer: Model,
+}
+impl SpatialDemo {
+	async fn new(client: &Arc<Client>) -> Result<Self> {
+		// let root = Spatial::create(client, client.get_root(), None, None, None, false).await?;
+		let root = Spatial::builder()
+			.client(client)
+			.spatial_parent(client.get_root())
+			.zoneable(true)
+			.build()
+			.await?;
+
+		let gem = Model::resource_builder()
+			.client(&client)
+			.spatial_parent(&root)
+			.resource(&Resource::new("libstardustxr", "gyro_gem.glb"))
+			.build()
+			.await?;
+		let ring_inner = Model::resource_builder()
+			.client(&client)
+			.spatial_parent(&root)
+			.resource(&Resource::new("libstardustxr", "gyro_inside.glb"))
+			.build()
+			.await?;
+		let ring_middle = Model::resource_builder()
+			.client(&client)
+			.spatial_parent(&ring_inner)
+			.resource(&Resource::new("libstardustxr", "gyro_middle.glb"))
+			.build()
+			.await?;
+		let ring_outer = Model::resource_builder()
+			.client(&client)
+			.spatial_parent(&ring_middle)
+			.resource(&Resource::new("libstardustxr", "gyro_outside.glb"))
+			.build()
+			.await?;
+
+		Ok(SpatialDemo {
+			client: client.clone(),
+			root,
+			gem,
+			ring_inner,
+			ring_middle,
+			ring_outer,
+		})
+	}
+}
+#[async_trait]
+impl LifeCycleHandler for SpatialDemo {
+	async fn logic_step(&self, info: LogicStepInfo) {
+		let elapsed = info.elapsed as f32;
+
+		let _ = tokio::join!(
+			self.gem.set_transform(
+				None,
+				None,
+				Some(Quat::from_scaled_axis(vec3(0.0, elapsed, 0.0)).into()),
+				None
+			),
+			self.ring_inner.set_transform(
+				None,
+				None,
+				Some(Quat::from_scaled_axis(vec3(0.0, 0.0, elapsed)).into()),
+				None
+			),
+			self.ring_middle.set_transform(
+				None,
+				None,
+				Some(Quat::from_scaled_axis(vec3(elapsed, 0.0, 0.0)).into()),
+				None
+			),
+			self.ring_outer.set_transform(
+				None,
+				None,
+				Some(Quat::from_scaled_axis(vec3(0.0, 0.0, elapsed)).into()),
+				None
+			),
+		);
+	}
+}
