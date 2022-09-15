f7270aa6db4a3b4f33660d83cc52a9949b5a8f38
Nova <technobaboo@gmail.com>
2022-09-14 23:08:52 -0400

    refactor(input): split InputData into new module

--- src/fusion/input/mod.rs
+++ src/fusion/input/mod.rs
@@ -1,6 +1,8 @@
+mod data;
 mod hand;
 mod pointer;
 
+pub use data::*;
 pub use pointer::*;
 // pub use hand::*;
 
@@ -11,82 +13,8 @@ use super::{
 	HandlerWrapper, WeakNodeRef, WeakWrapped,
 };
 use crate::values::{Quat, Vec3, QUAT_IDENTITY, VEC3_ZERO};
-use anyhow::{anyhow, bail};
-use ouroboros::self_referencing;
-use schemas::input::{root_as_input_data, InputDataRawT, InputDataT};
-use std::{
-	convert::{TryFrom, TryInto},
-	fmt::Debug,
-};
-
-#[derive(Debug, Clone)]
-pub enum InputDataType {
-	Pointer(Pointer),
-	// Hand(Hand),
-}
-
-#[self_referencing]
-pub struct InputData {
-	pub uid: String,
-	pub input: InputDataType,
-	pub distance: f32,
-	datamap_raw: Vec<u8>,
-
-	#[borrows(datamap_raw)]
-	#[not_covariant]
-	pub datamap: flexbuffers::MapReader<&'this [u8]>,
-}
-impl TryFrom<InputDataT> for InputData {
-	type Error = anyhow::Error;
-
-	fn try_from(input: InputDataT) -> Result<Self, Self::Error> {
-		InputData::try_new(
-			input.uid,
-			match input.input {
-				InputDataRawT::Pointer(pointer) => InputDataType::Pointer(Pointer::new(
-					pointer.origin.into(),
-					pointer.orientation.into(),
-					pointer.deepest_point.into(),
-				)),
-				InputDataRawT::Hand(_hand) => todo!("need hand struct format"),
-				_ => bail!("Invalid input type"),
-			},
-			input.distance,
-			input.datamap.ok_or_else(|| anyhow!("No datamap!"))?,
-			|datamap_raw| flexbuffers::Reader::get_root(datamap_raw.as_slice())?.get_map(),
-		)
-		.map_err(anyhow::Error::from)
-	}
-}
-impl Clone for InputData {
-	fn clone(&self) -> Self {
-		Self::new(
-			self.borrow_uid().clone(),
-			self.borrow_input().clone(),
-			*self.borrow_distance(),
-			self.borrow_datamap_raw().clone(),
-			|datamap_raw| {
-				flexbuffers::Reader::get_root(datamap_raw.as_slice())
-					.unwrap()
-					.get_map()
-					.unwrap()
-			},
-		)
-	}
-}
-impl Debug for InputData {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.debug_struct("InputData")
-			.field("uid", self.borrow_uid())
-			.field("input", self.borrow_input())
-			.field("distance", self.borrow_distance())
-			.field(
-				"datamap_raw",
-				&String::from_utf8_lossy(self.borrow_datamap_raw()).into_owned(),
-			)
-			.finish_non_exhaustive()
-	}
-}
+use schemas::input::root_as_input_data;
+use std::convert::TryInto;
 
 pub trait InputHandlerHandler: Send + Sync {
 	fn input(&mut self, input: InputData) -> bool;
