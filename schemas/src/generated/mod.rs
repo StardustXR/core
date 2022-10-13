f8855ecd02167253d351ce6aff7271f07211fb5f
Nova <technobaboo@gmail.com>
2022-10-12 20:17:11 -0400

    fix(schemas): features in regular dependencies

--- schemas/src/generated/mod.rs
+++ schemas/src/generated/mod.rs
@@ -1,24 +1,24 @@
-pub mod common {
+pub mod common { 
 	pub use self::stardust_xr::*;
-	include!("common.rs");
+	include!("common.rs"); 
 }
-pub mod input_tip {
+pub mod input_tip { 
 	pub use self::stardust_xr::*;
-	include!("input_tip.rs");
+	include!("input_tip.rs"); 
 }
-pub mod input_pointer {
+pub mod input_pointer { 
 	pub use self::stardust_xr::*;
-	include!("input_pointer.rs");
+	include!("input_pointer.rs"); 
 }
-pub mod input {
+pub mod input { 
 	pub use self::stardust_xr::*;
-	include!("input.rs");
+	include!("input.rs"); 
 }
-pub mod input_hand {
+pub mod input_hand { 
 	pub use self::stardust_xr::*;
-	include!("input_hand.rs");
+	include!("input_hand.rs"); 
 }
-pub mod message {
+pub mod message { 
 	pub use self::stardust_xr::*;
-	include!("message.rs");
+	include!("message.rs"); 
 }
