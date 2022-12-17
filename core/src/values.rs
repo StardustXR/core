64cc7d13f1876e86a6b45a999714c1acb86421d3
Nova <technobaboo@gmail.com>
2022-12-17 01:55:04 -0500

    feat(values): transform constructors

--- core/src/values.rs
+++ core/src/values.rs
@@ -8,6 +8,70 @@ pub struct Transform {
 	pub rotation: Quaternion<f32>,
 	pub scale: Vector3<f32>,
 }
+impl Transform {
+	pub fn from_position(position: impl Into<Vector3<f32>>) -> Self {
+		Transform {
+			position: position.into(),
+			..Default::default()
+		}
+	}
+	pub fn from_rotation(rotation: impl Into<Quaternion<f32>>) -> Self {
+		Transform {
+			rotation: rotation.into(),
+			..Default::default()
+		}
+	}
+	pub fn from_scale(scale: impl Into<Vector3<f32>>) -> Self {
+		Transform {
+			scale: scale.into(),
+			..Default::default()
+		}
+	}
+
+	pub fn from_position_rotation(
+		position: impl Into<Vector3<f32>>,
+		rotation: impl Into<Quaternion<f32>>,
+	) -> Self {
+		Transform {
+			position: position.into(),
+			rotation: rotation.into(),
+			..Default::default()
+		}
+	}
+	pub fn from_rotation_scale(
+		rotation: impl Into<Quaternion<f32>>,
+		scale: impl Into<Vector3<f32>>,
+	) -> Self {
+		Transform {
+			rotation: rotation.into(),
+			scale: scale.into(),
+			..Default::default()
+		}
+	}
+
+	pub fn from_position_scale(
+		position: impl Into<Vector3<f32>>,
+		scale: impl Into<Vector3<f32>>,
+	) -> Self {
+		Transform {
+			position: position.into(),
+			scale: scale.into(),
+			..Default::default()
+		}
+	}
+
+	pub fn from_position_rotation_scale(
+		position: impl Into<Vector3<f32>>,
+		rotation: impl Into<Quaternion<f32>>,
+		scale: impl Into<Vector3<f32>>,
+	) -> Self {
+		Transform {
+			position: position.into(),
+			rotation: rotation.into(),
+			scale: scale.into(),
+		}
+	}
+}
 impl Default for Transform {
 	fn default() -> Self {
 		Self {
