2150359a7cb96538c80d980b46f679e902356932
Nova <technobaboo@gmail.com>
2024-02-03 04:14:53 -0500

    refactor(core): values

--- core/src/values.rs
+++ core/src/values.rs
@@ -1,111 +1,84 @@
-use mint::{Quaternion, Vector3};
-use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
 
-/// A box
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-pub struct Box {
-	pub center: Vector3<f32>,
-	pub size: Vector3<f32>,
+pub use color::rgba_linear;
+use color::{color_space::LinearRgb, Rgba};
+use serde::{Deserialize, Serialize, Serializer};
+
+pub use stardust_xr_schemas::flex::Datamap;
+
+pub type Color = Rgba<f32, LinearRgb>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+/// An identifier to a resource, such as a sound or
+pub enum ResourceID {
+	/// An absolute path to a resource, not themed at all.
+	/// You should only use this for content not included with your client.
+	Direct(PathBuf),
+	/// A resource that is relative to a prefix, meant for resources that are included with the client.
+	/// Allows switching of prefix by the server as well to theme clients.
+	///
+	/// # Example
+	/// ```
+	/// use stardust_xr_fusion::{drawable::Model, resource::ResourceID};
+	///
+	/// // For a client named "star"
+	/// let model_resource = ResourceID::new_namespaced("star", "icon");
+	/// // Build a model at "[prefix]/star/icon.glb" if it exists
+	/// let model = Model::create(client.get_root(), Transform::none(), model_resource).unwrap();
+	/// ```
+	Namespaced {
+		/// Group that this resource is in, generally the client or library's name.
+		namespace: String,
+		/// Path inside the namespace for the exact file. Leave out the extension and ensure no leading slash.
+		path: String,
+	},
 }
-
-/// Simple transform
-#[derive(Default, Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-pub struct Transform {
-	pub position: Option<Vector3<f32>>,
-	pub rotation: Option<Quaternion<f32>>,
-	pub scale: Option<Vector3<f32>>,
+impl ResourceID {
+	pub fn new_direct(path: impl AsRef<Path>) -> std::io::Result<ResourceID> {
+		let path = path.as_ref();
+		path.try_exists()?;
+		if !path.is_absolute() {
+			return Err(std::io::Error::new(
+				std::io::ErrorKind::NotFound,
+				"Path is not absolute",
+			));
+		}
+		Ok(ResourceID::Direct(path.to_path_buf()))
+	}
+	pub fn new_namespaced(namespace: &str, path: &str) -> Self {
+		ResourceID::Namespaced {
+			namespace: namespace.to_string(),
+			path: path.to_string(),
+		}
+	}
+	pub(crate) fn parse(&self) -> String {
+		match self {
+			ResourceID::Direct(p) => p.to_str().unwrap().to_string(),
+			ResourceID::Namespaced { namespace, path } => {
+				format!("{}:{}", namespace, path)
+			}
+		}
+	}
 }
-impl Transform {
-	pub const fn none() -> Self {
-		Transform {
-			position: None,
-			rotation: None,
-			scale: None,
-		}
+impl Serialize for ResourceID {
+	fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+		serializer.serialize_str(&self.parse())
 	}
-	pub const fn identity() -> Self {
-		Transform {
-			position: Some(Vector3 {
-				x: 0.0,
-				y: 0.0,
-				z: 0.0,
-			}),
-			rotation: Some(Quaternion {
-				v: Vector3 {
-					x: 0.0,
-					y: 0.0,
-					z: 0.0,
-				},
-				s: 1.0,
-			}),
-			scale: Some(Vector3 {
-				x: 1.0,
-				y: 1.0,
-				z: 1.0,
-			}),
-		}
-	}
-
-	pub fn from_position(position: impl Into<Vector3<f32>>) -> Self {
-		Transform {
-			position: Some(position.into()),
-			..Default::default()
-		}
-	}
-	pub fn from_rotation(rotation: impl Into<Quaternion<f32>>) -> Self {
-		Transform {
-			rotation: Some(rotation.into()),
-			..Default::default()
-		}
-	}
-	pub fn from_scale(scale: impl Into<Vector3<f32>>) -> Self {
-		Transform {
-			scale: Some(scale.into()),
-			..Default::default()
-		}
-	}
-
-	pub fn from_position_rotation(
-		position: impl Into<Vector3<f32>>,
-		rotation: impl Into<Quaternion<f32>>,
-	) -> Self {
-		Transform {
-			position: Some(position.into()),
-			rotation: Some(rotation.into()),
-			..Default::default()
-		}
-	}
-	pub fn from_rotation_scale(
-		rotation: impl Into<Quaternion<f32>>,
-		scale: impl Into<Vector3<f32>>,
-	) -> Self {
-		Transform {
-			rotation: Some(rotation.into()),
-			scale: Some(scale.into()),
-			..Default::default()
-		}
-	}
-
-	pub fn from_position_scale(
-		position: impl Into<Vector3<f32>>,
-		scale: impl Into<Vector3<f32>>,
-	) -> Self {
-		Transform {
-			position: Some(position.into()),
-			scale: Some(scale.into()),
-			..Default::default()
-		}
-	}
-
-	pub fn from_position_rotation_scale(
-		position: impl Into<Vector3<f32>>,
-		rotation: impl Into<Quaternion<f32>>,
-		scale: impl Into<Vector3<f32>>,
-	) -> Self {
-		Transform {
-			position: Some(position.into()),
-			rotation: Some(rotation.into()),
-			scale: Some(scale.into()),
-		}
+}
+impl<'de> Deserialize<'de> for ResourceID {
+	fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+		let v = String::deserialize(deserializer)?;
+		Ok(if v.starts_with('/') {
+			let path = PathBuf::from(v);
+			path.metadata().map_err(serde::de::Error::custom)?;
+			ResourceID::Direct(path)
+		} else if let Some((namespace, path)) = v.split_once(':') {
+			ResourceID::Namespaced {
+				namespace: namespace.to_string(),
+				path: path.to_string(),
+			}
+		} else {
+			return Err(serde::de::Error::custom("Invalid format for string"));
+		})
 	}
 }
