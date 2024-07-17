8a1b52bfc3542d4aab47d4fa051eb06bcb346a25
Nova <technobaboo@gmail.com>
2024-07-17 13:16:31 -0400

    fix(dbus): make sure object part starts with letter

--- schemas/src/dbus/mod.rs
+++ schemas/src/dbus/mod.rs
@@ -6,7 +6,7 @@ use zbus::{conn, fdo::ObjectManager, proxy::Builder, Connection, Result};
 pub async fn connect_client() -> zbus::Result<zbus::Connection> {
 	conn::Builder::session()?
 		.serve_at("/", ObjectManager)?
-		.name("org.stardustxr.Object.".to_string() + &nanoid::nanoid!())?
+		.name("org.stardustxr.Object.o".to_string() + &nanoid::nanoid!())?
 		.build()
 		.await
 }
