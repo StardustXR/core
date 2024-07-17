146211eb4cfde2b2fa941cf20664d6f8fcae092e
Nova <technobaboo@gmail.com>
2024-07-17 13:24:14 -0400

    fix(dbus): connect_client better random string

--- schemas/src/dbus/mod.rs
+++ schemas/src/dbus/mod.rs
@@ -6,7 +6,20 @@ use zbus::{conn, fdo::ObjectManager, proxy::Builder, Connection, Result};
 pub async fn connect_client() -> zbus::Result<zbus::Connection> {
 	conn::Builder::session()?
 		.serve_at("/", ObjectManager)?
-		.name("org.stardustxr.Object.o".to_string() + &nanoid::nanoid!())?
+		.name(
+			"org.stardustxr.Object.o".to_string()
+				+ &random_string::generate(
+					10,
+					"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+				),
+		)?
 		.build()
 		.await
+}
+
+#[tokio::test]
+async fn connect_client_test() {
+	for _ in 0..32 {
+		let _ = connect_client().await.unwrap();
+	}
 }
