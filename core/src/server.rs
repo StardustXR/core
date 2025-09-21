2a943a66a97e055da7da8889d4f1996d4f466ad0
Nova <technobaboo@gmail.com>
2025-09-21 12:37:41 -0700

    refactor(core/server): remove cluFlock

--- core/src/server.rs
+++ core/src/server.rs
@@ -1,40 +1,54 @@
-use cluFlock::ExclusiveFlock;
 use dirs::runtime_dir;
 use std::{
 	fs::{self, File},
 	path::PathBuf,
 };
 
-/// Get the lowest numbered socket path not taken by another server, if available.
-pub fn get_free_socket_path() -> Option<PathBuf> {
-	// Get the base XDG directories
-	let runtime_dir = runtime_dir()?;
-	let mut socket_number = 0_u8;
+pub struct LockedSocket {
+	pub socket_path: PathBuf,
+	_file: File,
+}
+impl LockedSocket {
+	pub fn get_free() -> Option<Self> {
+		// Get the base XDG directories
+		let runtime_dir = runtime_dir()?;
+		let mut socket_number = 0_u8;
 
-	loop {
-		if socket_number > 32 {
-			return None;
-		}
-		let socket_path = runtime_dir.join(format!("stardust-{socket_number}"));
-		let socket_lock_path = runtime_dir.join(format!("stardust-{socket_number}.lock"));
-		match File::create(socket_lock_path) {
-			Err(_) => continue,
-			Ok(file) => match file.try_lock() {
-				Err(_) => socket_number += 1,
-				Ok(_) => match fs::remove_file(socket_path.clone()) {
-					Ok(_) => return Some(socket_path),
-					Err(err) => match err.kind() {
-						std::io::ErrorKind::NotFound => return Some(socket_path),
-						_ => continue,
+		loop {
+			if socket_number > 32 {
+				return None;
+			}
+			let socket_path = runtime_dir.join(format!("stardust-{socket_number}"));
+			let socket_lock_path = runtime_dir.join(format!("stardust-{socket_number}.lock"));
+			match File::create(socket_lock_path) {
+				Err(_) => continue,
+				Ok(file) => match file.try_lock() {
+					Err(_) => socket_number += 1,
+					Ok(_) => match fs::remove_file(socket_path.clone()) {
+						Ok(_) => {
+							return Some(Self {
+								socket_path,
+								_file: file,
+							});
+						}
+						Err(err) => match err.kind() {
+							std::io::ErrorKind::NotFound => {
+								return Some(Self {
+									socket_path,
+									_file: file,
+								});
+							}
+							_ => continue,
+						},
 					},
 				},
-			},
+			}
 		}
 	}
 }
 
 #[test]
 fn server_get_free_socket_path() {
-	let socket_path = get_free_socket_path().expect("Unable to set up socket!");
-	println!("Socket is free up at {}", socket_path.display());
+	let socket_path = LockedSocket::get_free().expect("Unable to set up socket!");
+	println!("Socket is free up at {}", socket_path.socket_path.display());
 }
