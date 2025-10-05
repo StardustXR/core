c716d332d682306291db16e9753cb7cb9dcc5a4c
Nova <technobaboo@gmail.com>
2025-10-05 15:27:33 -0700

    feat(schemas): proper reparent lock

--- schemas/src/dbus/interfaces.rs
+++ schemas/src/dbus/interfaces.rs
@@ -31,16 +31,19 @@ pub trait PlaySpace {
 #[zbus::proxy(interface = "org.stardustxr.Reparentable")]
 /// You need to implement at least SpatialRef but optionally FieldRef
 pub trait Reparentable {
-	/// Ask the zoneable to parent itself to the given SpatialRef
+	/// Ask the reparentable to parent itself to the given SpatialRef
 	fn parent(&self, new_parent: u64) -> Result<()>;
-	/// Set the transform of the zoneable relative to the given SpatialRef to zero
+	/// Ask the reparentable to unparent itself from the given SpatialRef
+	fn unparent(&self) -> Result<()>;
+	/// Set the transform of the reparentable relative to the given SpatialRef to zero
 	fn reset_transform(&self, spatial_ref: u64) -> Result<()>;
 }
 #[zbus::proxy(interface = "org.stardustxr.ReparentLock")]
-/// You need to implement Zoneable
+/// You need to implement reparentable
 pub trait ReparentLock {
-	fn capture(&self) -> Result<()>;
-	fn release(&self) -> Result<()>;
+	/// Ask to make it so nothing else can reparent this
+	fn lock(&self) -> Result<()>;
+	fn unlock(&self) -> Result<()>;
 }
 
 impl_queryable_for_proxy!(
