36ee250dd23223e79e3a9e3821aecaf76b9cdc73
Nova <technobaboo@gmail.com>
2026-05-06 23:54:52 -0700

    fix(protocol): public

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,14 +1,21 @@
-#![allow(clippy::derivable_impls)]
+#![allow(clippy::derivable_impls, unused)]
 
 mod protocol;
 pub use protocol::*;
 
 mod conversions;
+pub use conversions::*;
 mod drawable_ext;
+pub use drawable_ext::*;
 mod field_ext;
+pub use field_ext::*;
 mod query_ext;
+pub use query_ext::*;
 mod spatial_ext;
+pub use spatial_ext::*;
 mod suis_ext;
+pub use suis_ext::*;
 mod types_ext;
+pub use types_ext::*;
 
 pub mod dir;
