b20d313554b77422099e89ee9b08b17e3c776351
Schmarni <marnistromer@gmail.com>
2026-05-30 08:12:27 +0200

    fix: push the file
    Signed-off-by: Schmarni <marnistromer@gmail.com>

---
+++ protocol/src/dmatex.rs
@@ -0,0 +1,20 @@
+use std::{error::Error, fmt::Display};
+
+use crate::protocol::dmatex::DmatexImportError;
+
+impl Error for DmatexImportError {}
+impl Display for DmatexImportError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			DmatexImportError::InvalidSize => f.write_str("Invalid size"),
+			DmatexImportError::InvalidFormat => f.write_str("Invalid format"),
+			DmatexImportError::UnsupportedArrayLayers {
+				max_supported_layers,
+			} => f.write_str(&format!(
+				"Unsupported amount of array layers, max supported layers: {max_supported_layers}"
+			)),
+			DmatexImportError::InvalidPlanes => f.write_str("Invalid planes"),
+			DmatexImportError::InvalidTimelineFd => f.write_str("Invalide timeline syncobj"),
+		}
+	}
+}
