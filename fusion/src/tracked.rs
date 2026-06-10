4744a8390eff98ffe58be0b5c29167fe4fafd09e
Schmarni <marnistromer@gmail.com>
2026-06-10 20:06:09 +0200

    feat(fusion/tracked): add helpers for getting the hmd and stage tracked
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- fusion/src/tracked.rs
+++ fusion/src/tracked.rs
@@ -1,1 +1,91 @@
+use std::{fs, future};
+
+use crate::{Error, Result};
+use gluon::Handler;
 pub use stardust_xr_protocol::tracked::*;
+use stardust_xr_protocol::{
+	client::ClientHandler, dir, spatial::SpatialRef, types::ResourceLoadError,
+};
+
+use crate::client::Client;
+
+pub trait TrackedExt {
+	fn hmd(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<Tracked>> + Send;
+	fn hmd_spatial(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<SpatialRef>> + Send;
+	fn stage(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<Tracked>> + Send;
+	fn stage_spatial(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<SpatialRef>> + Send;
+}
+impl TrackedExt for Tracked {
+	fn hmd(client: &Client<impl ClientHandler>) -> impl Future<Output = Result<Tracked>> {
+		get_tracked(client, "stardust-hmd")
+	}
+	fn stage(client: &Client<impl ClientHandler>) -> impl Future<Output = Result<Tracked>> {
+		get_tracked(client, "stardust-stage")
+	}
+
+	fn hmd_spatial(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<SpatialRef>> + Send {
+		get_tracked_spatial(client, "stardust-hmd")
+	}
+
+	fn stage_spatial(
+		client: &Client<impl ClientHandler>,
+	) -> impl std::future::Future<Output = Result<SpatialRef>> + Send {
+		get_tracked_spatial(client, "stardust-stage")
+	}
+}
+
+async fn get_tracked(client: &Client<impl ClientHandler>, name: &str) -> Result<Tracked> {
+	// completely incorrect error, but there isn't really a better one
+	let path = dir::find_pion_file(name).ok_or(ResourceLoadError::NotFound)?;
+	let file = fs::OpenOptions::new()
+		.read(true)
+		.write(true)
+		.create(false)
+		.open(&path)
+		.map_err(Error::PionFile)?;
+	let handle = client
+		.pion_device()
+		.get_binder_ref_from_file(file)
+		.await
+		// even more incorrect error, but there isn't really a better one
+		.map_err(|_| ResourceLoadError::InvalidRef)?;
+	// TODO: do proper checks to make sure this is actually a tracked
+	Ok(Tracked::from_object_or_ref(handle))
+}
+
+async fn get_tracked_spatial(
+	client: &Client<impl ClientHandler>,
+	name: &str,
+) -> Result<SpatialRef> {
+	let tracked = get_tracked(client, name).await?;
+	let handler = client
+		.pion_device()
+		.register_object(TrackedHandlerNoop)
+		.to_service();
+	let (spatial, _, _) = tracked
+		.get(TrackedStateReceiver::from_handler(&handler))
+		.await?;
+	Ok(spatial)
+}
+
+#[derive(Handler, Debug)]
+struct TrackedHandlerNoop;
+impl TrackedStateReceiverHandler for TrackedHandlerNoop {
+	fn tracked(
+		&self,
+		_ctx: gluon::Context,
+		_tracked: bool,
+	) -> impl Future<Output = ()> + Send + Sync {
+		future::ready(())
+	}
+}
